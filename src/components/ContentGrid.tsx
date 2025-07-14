6bd6a7ea862dc0f2c0bfaf33d3c58900577942a4
Danish Khan <khand000786@gmail.com>
2025-07-14 18:50:41 +0000

    Add some margin to the containers of movie and tv show catalog so that,

--- src/components/ContentGrid.tsx
+++ src/components/ContentGrid.tsx
@@ -14,7 +14,9 @@ export function ContentGrid({ items, type }: ContentGridProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
       {items.map((item) => (
-        <ContentCard key={item.id} item={item} type={type} />
+        <div key={item.id} className="mt-2">
+          <ContentCard item={item} type={type} />
+        </div>
       ))}
     </div>
   );
