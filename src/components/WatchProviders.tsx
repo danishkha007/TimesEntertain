03547008c26c9bfab6634ea855dd6e5488a03c9d
Danish Khan <khand000786@gmail.com>
2025-07-18 07:07:56 +0000

    images are not clear, fetch and store in public directory under ott logo

--- src/components/WatchProviders.tsx
+++ src/components/WatchProviders.tsx
@@ -18,7 +18,7 @@ interface WatchProvidersProps {
     className?: string;
 }
 
-const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w45';
+const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w92';
 
 const CATEGORY_MAP: { [key in keyof OttPlatformDetails]: string } = {
   flatrate: "Stream",
