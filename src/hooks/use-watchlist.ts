4d43f16cfb9634226fca1ee33e4d1e1638d733b1
Danish Khan <khand000786@gmail.com>
2025-08-15 13:04:01 +0000

    Why on mywatchlist page only the placeholder image is showing

--- src/hooks/use-watchlist.ts
+++ src/hooks/use-watchlist.ts
@@ -41,6 +41,10 @@ export function useWatchlist() {
       
       const itemToAdd: WatchlistItem = {
           ...item,
+          poster_url: 'poster_url' in item ? item.poster_url : undefined,
+          posterUrl: 'posterUrl' in item ? item.posterUrl : undefined,
+          genres: 'genres' in item ? item.genres : ('genre' in item ? item.genre : []),
+          imdb_rating: 'imdb_rating' in item ? item.imdb_rating : ('rating' in item ? item.rating : 0),
           itemType: type,
           slug,
       }
